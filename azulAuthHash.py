0678d605e5f0a82544f9d66817c13712aabe7638
Manuel Gonzalez <manuel@softnet.do>
2022-03-22 13:04:33 -0400

    Second Script: - using unicode() to generate a UTF-16LE string - using hashlib to generate a hash sha512 form a UTF-16LE string
    This Hash SHA512 is required by Azul Payment Gateway from Dominican Republic (Banco Popular Dominicano BPD)  to be use as Authentication of their FORM.

---
+++ azulAuthHash.py
@@ -0,0 +1,47 @@
+import hashlib
+
+class azulAuthHash():
+
+    def _generate_hash(self, args):
+
+        args_string = ''.join(str(v) for v in args.values())
+        # args_string = ','.join(map(str, args.values()))  # this way works too
+        utf_string = args_string.encode('UTF-16LE')
+        hash_sha512_string = hashlib.sha512(utf_string).hexdigest()
+
+        ''''
+        Azul documentation as December 2020, says that we need do this:
+        * 1 - concatenating a few fields (those in dict format at method: _getAzulFormFields())
+        * 2 - calculate a hash sha512
+        * 3 - convert the hash to hexadecimal string
+        '''
+
+        return hash_sha512_string
+
+    def _getAzulFormFields(self):
+
+        return {
+            'MerchantId'        : '455593373663',
+            'MerchantName'      : 'Fast Food Services',
+            'MerchantType'      : 'Fast Food Industry',
+            'CurrencyCode'      : '$',
+            'OrderNumber'       : '001234',
+            'Amount'            : '100000',
+            'ITBIS'             : '18000',
+            'ApprovedUrl'       : 'softnet.do/success',
+            'DeclinedUrl'       : 'softnet.do/declined',
+            'CancelUrl'         : 'softnet.do/canceled',
+            'UseCustomField1'   : '1',
+            'CustomField1Label' : 'Foo',
+            'CustomField1Value' : 'Bar',
+            'UseCustomField2'   : '0',
+            'CustomField2Label' : '',
+            'CustomField2Value' : '',
+            'AuthKey'           : 'asdhakjshdkjasdasmndajksdkjaskldga8odya9d8yoasyd98asdyaisdhoaisyd0a8sydoashd8oasydoiahdpiashd09ayusidhaos8dy0a8dya08syd0a8ssdsax', # ***** WARNING! *****, DON'T SEND THIS FIELD IN THE POST FORM IN CLIENT SIDE. Tips: unset($args['AuthKey'])
+        }
+
+
+azulAuthHash = azulAuthHash()
+data = azulAuthHash._getAzulFormFields()
+authHash = azulAuthHash._generate_hash(data)
+print (authHash)
