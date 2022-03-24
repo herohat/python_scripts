66dfc2c231d76a0bb3fd2b66306d7c8e16bdd752
Manuel Gonzalez <manuel@softnet.do>
2022-03-23 20:30:41 -0400

    Second Script: - Fix for order needed

--- azulAuthHash.py
+++ azulAuthHash.py
@@ -1,11 +1,12 @@
 import hashlib
+from collections import OrderedDict
 
 class azulAuthHash():
 
     def _generate_hash(self, args):
 
         args_string = ''.join(str(v) for v in args.values())
-        # args_string = ','.join(map(str, args.values()))  # this way works too
+        # args_string = ''.join(map(str, args.values()))  # this way works too
         utf_string = args_string.encode('UTF-16LE')
         hash_sha512_string = hashlib.sha512(utf_string).hexdigest()
 
@@ -20,25 +21,25 @@ class azulAuthHash():
 
     def _getAzulFormFields(self):
 
-        return {
-            'MerchantId'        : '455593373663',
-            'MerchantName'      : 'Fast Food Services',
-            'MerchantType'      : 'Fast Food Industry',
-            'CurrencyCode'      : '$',
-            'OrderNumber'       : '001234',
-            'Amount'            : '100000',
-            'ITBIS'             : '18000',
-            'ApprovedUrl'       : 'softnet.do/success',
-            'DeclinedUrl'       : 'softnet.do/declined',
-            'CancelUrl'         : 'softnet.do/canceled',
-            'UseCustomField1'   : '1',
-            'CustomField1Label' : 'Foo',
-            'CustomField1Value' : 'Bar',
-            'UseCustomField2'   : '0',
-            'CustomField2Label' : '',
-            'CustomField2Value' : '',
-            'AuthKey'           : 'asdhakjshdkjasdasmndajksdkjaskldga8odya9d8yoasyd98asdyaisdhoaisyd0a8sydoashd8oasydoiahdpiashd09ayusidhaos8dy0a8dya08syd0a8ssdsax', # ***** WARNING! *****, DON'T SEND THIS FIELD IN THE POST FORM IN CLIENT SIDE. Tips: unset($args['AuthKey'])
-        }
+        return OrderedDict([
+                            ('MerchantId', str('455593373663')),
+                            ('MerchantName', 'Fast Food Services'),
+                            ('MerchantType', 'Fast Food Industry'),
+                            ('CurrencyCode', '$'),
+                            ('OrderNumber', '001234'),
+                            ('Amount', str('100000')),
+                            ('ITBIS', str('18000')),
+                            ('ApprovedUrl', 'softnet.do/success'),
+                            ('DeclinedUrl', 'softnet.do/declined'),
+                            ('CancelUrl', 'softnet.do/canceled'),
+                            ('UseCustomField1', str('1')),
+                            ('CustomField1Label', 'Foo'),
+                            ('CustomField1Value', 'Bar'),
+                            ('UseCustomField2', str('0')),
+                            ('CustomField2Label', ''),
+                            ('CustomField2Value', ''),
+                            ('AuthKey', 'asdhakjshdkjasdasmndajksdkjaskldga8odya9d8yoasyd98asdyaisdhoaisyd0a8sydoashd8oasydoiahdpiashd09ayusidhaos8dy0a8dya08syd0a8ssdsax') # ***** WARNING! *****, DON'T SEND THIS FIELD IN THE POST FORM IN CLIENT SIDE. Tips: unset($args['AuthKey'])
+                            ])        
 
 
 azulAuthHash = azulAuthHash()
